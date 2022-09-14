03d17f4ee585afb1ea0b6576e81f3406651f2c01
s8189882 <s8189882@gmail.com>
2022-09-14 14:50:02 +0530

    Create main branch to validate pincode

---
+++ RegEx Pincode/src/regex/pincode/validation/PinCodeValidation.java
@@ -0,0 +1,8 @@
+package regex.pincode.validation;
+
+public class PinCodeValidation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Regular Expressions program to validate PIN code!\n");
+	}
+}
